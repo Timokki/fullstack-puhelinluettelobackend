71c3c7b74be3dd087d8409e3c52ed35daa2512b9
Timo Keränen <timo@timokeranen.com>
2021-10-01 22:30:46 +0300

    Tehtävä 3.12 toimii

--- mongo.js
+++ mongo.js
@@ -1,37 +1,46 @@
+console.log("mongo.js käynnistyi.")
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
+const password = process.argv[2]
 
-// Salasan on käynnistyksessä toisena argumenttinä
-const password = process.argv[0]
-const nameArg = process.argv[1]
-const numberArg = process.argv[2]
+//console.log("process.argv.length: ", process.argv.length)
+const addingNew = (process.argv.length>3) ? true : false; 
 
-//mongodb+srv://fullstack:<password>@cluster0.6kjcn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const url = `mongodb+srv://fullstack:${password}@cluster0.6kjcn.mongodb.net/myFirstDatabase?retryWrites=true`
 
-  mongoose.connect(url)
+mongoose.connect(url)
 
 // Scheema kertoo mongooselle, miten oliot tulee tallettaa tietokantaan.
-const noteSchema = new mongoose.Schema({
+const personSchema = new mongoose.Schema({
   name: String,
   number: String
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
+//console.log(addingNew)
+if (addingNew)
+{
+  const nameArg = process.argv[3]
+  const numberArg = process.argv[4]
 
-// Muistiinpano luodaan mongoose.model konstruktorifunktion avulla.
-const note = new Note({
-  neme: nameArg,
-  number: numberArg
-})
+  // Muistiinpano luodaan mongoose.model konstruktorifunktion avulla.
+  const person = new Person({
+    name: nameArg,
+    number: numberArg
+  })
 
-// Koska oliot on luotu modelien konstruktorifunktiolla, niillä on kaikki modelien 
-// ominaisuudet eli joukko metodeja, joiden avulla olioita voidaan mm. tallettaa tietokantaan.
-note.save().then(response => {
-  console.log('note saved!')
-  mongoose.connection.close() // TÄRKEÄ, yhteys tietokantaan suljetaan vasta kun tallennus on valmis.
-})
+  // Koska oliot on luotu modelien konstruktorifunktiolla, niillä on kaikki modelien 
+  // ominaisuudet eli joukko metodeja, joiden avulla olioita voidaan mm. tallettaa tietokantaan.
+  person.save().then(response => {
+    console.log('person saved!')
+    mongoose.connection.close() // TÄRKEÄ, yhteys tietokantaan suljetaan vasta kun tallennus on valmis.
+  })
+} else {
+  Person.find({}).then(result => {
+    console.log("Phonebook:")
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+}
