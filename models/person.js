73f26d0cbbc1d3ec57f62a1a61da85c08b076572
Timo Keränen <timo@timokeranen.com>
2021-10-26 23:25:15 +0300

    Person lisätty main haaraan

---
+++ models/person.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+//const url = `mongodb+srv://fullstack:${password}@cluster0.6kjcn.mongodb.net/myFirstDatabase?retryWrites=true`
+
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
+mongoose.connect(url)
+
+// Scheema kertoo mongooselle, miten oliot tulee tallettaa tietokantaan.
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+personSchema.set('toJSON', {
+  transform: (document, responsePerson) => {
+    responsePerson.id = responsePerson._id.toString()
+    delete responsePerson._id
+    delete responsePerson.__v
+  }
+})
+
+mongoose.connect(url).then(result => {
+    console.log('connected to MongoDB')
+  })  
+.catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+/*Moduulin ulos näkyvä osa määritellään asettamalla arvo muuttujalle module.exports. 
+Asetamme arvoksi modelin Person. Muut moduulin sisällä määritellyt asiat, 
+esim. muuttujat mongoose ja url eivät näy moduulin käyttäjälle.*/
+module.exports = mongoose.model('Person', personSchema)
